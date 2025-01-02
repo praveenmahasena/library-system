aa015dc90dc364c4b6aecac194f8266c7ca4520c
praveenmahasena <praveenmahasena647@gmail.com>
2025-01-02 09:22:04 -0500

    db connection done

--- internal/app.go
+++ internal/app.go
@@ -1,10 +1,25 @@
 package internal
 
 import (
+	"errors"
+
+	db "github.com/praveenmahasena/libsys/internal/database"
 	"github.com/praveenmahasena/libsys/internal/server"
+	"github.com/spf13/viper"
 )
 
 func Start() error {
+	viper.SetConfigFile(".env")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return errors.New("Error during reading config file")
+	}
+
+	dbErr := db.Connect()
+
+	if dbErr != nil {
+		return dbErr
+	}
 
 	ns := server.New(":42069")
 
--- internal/database/connect.go
+++ internal/database/connect.go
@@ -1,8 +1,47 @@
 package database
 
 import (
+	"errors"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
-func Connect()(*sqlx.DB,error){
-	return nil,nil
+// I really do not like the approach of making the connection as global var
+// gotta refactor again
+
+var Con *sqlx.DB
+
+const (
+	hostParam     = "HOST"
+	portParam     = "PORT"
+	userParam     = "USER"
+	passwordParam = "PASSWORD"
+	dbNameParam   = "DBNAME"
+	sslModeParam  = "SSLMODE"
+)
+
+func getParams() string {
+	host := viper.GetString(hostParam)
+	port := viper.GetString(portParam)
+	user := viper.GetString(userParam)
+	password := viper.GetString(passwordParam)
+	dbName := viper.GetString(dbNameParam)
+	sslmode := viper.GetString(sslModeParam)
+	return fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=%v", host, port, user, password, dbName, sslmode)
+}
+
+func Connect() error {
+	var err error
+	uri := getParams()
+	Con, err = sqlx.Connect("postgres", uri)
+	if err != nil {
+		return errors.New("could not connect to database")
+	}
+	if err := Con.Ping(); err != nil {
+		return errors.New("issue during connection testing")
+	}
+	return nil
 }
